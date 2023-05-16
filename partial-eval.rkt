8d71d2e804e5393e2810f5a60a1be9617844f22d
Thales Magalhães <thls.mglhs@gmail.com>
2023-05-15 22:25:03 -0300

    Evaluate expressions as-is (i.e. without `expand`-ing them first)

--- partial-eval.rkt
+++ partial-eval.rkt
@@ -2,49 +2,37 @@
 
 (provide partial-eval)
 
-(require syntax/id-table)
+(require syntax/id-table syntax/stx)
 
-(struct closure (arg-ids expr env) #:transparent)
+(struct closure (arg-ids expr env))
 
 (define make-empty-environment make-immutable-bound-id-table)
-(define bind-id dict-set)
-(define lookup-id dict-ref)
-
-(define-namespace-anchor anchor)
+(define bind dict-set)
+(define lookup dict-ref)
 
 (define (partial-eval expr)
-  (parameterize ([current-namespace (namespace-anchor->namespace anchor)])
-    (partial-eval-syntax (datum->syntax #f expr))))
+  (partial-eval-syntax (datum->syntax #f expr)))
 
 (define (partial-eval-syntax stx)
-  (peval (expand stx) (make-empty-environment)))
+  (peval stx (make-empty-environment)))
 
-(define (lam? stx)
-  (define lam (car (syntax->list (cadr (syntax->list (expand '(lambda (x) x)))))))
-  (and (identifier? stx) (free-identifier=? stx lam)))
-
-(define (app? stx)
-  (define app (car (syntax->list (expand '(f x)))))
-  (and (identifier? stx) (free-identifier=? stx app)))
-
-(define (exp? stx)
-  (define exp #'#%expression)
-  (and (identifier? stx) (free-identifier=? stx exp)))
+(define (literal? stx)
+  (define v (syntax->datum stx))
+  (or (boolean? v) (number? v)))
 
 (define (peval stx env)
-  (syntax-case stx (if quote let-values +)
-    [(exp expr) (exp? #'exp) (peval #'expr env)]
-    [id (identifier? #'id) (lookup-id env #'id)]
-    [(lam (arg-id ...) expr) (lam? #'lam)
+  (syntax-case* stx (quote lambda if let +) module-or-top-identifier=?
+    [id (identifier? #'id) (lookup env #'id)]
+    [datum (literal? #'datum) (syntax->datum #'datum)]
+    [(quote datum) (syntax->datum #'datum)]
+    [(lambda (arg-id ...) expr)
      (closure (syntax->list #'(arg-id ...)) #'expr env)]
     [(if pred true-expr false-expr)
      (peval (if (peval #'pred env) #'true-expr #'false-expr) env)]
-    [(quote datum) (syntax->datum #'datum)]
-    [(let-values ([(id) expr]) body) (identifier? #'id)
-     (peval #'body (bind-id env #'id (peval #'expr env)))]
-    [(app + expr1 expr2) (app? #'app)
-     (+ (peval #'expr1 env) (peval #'expr2 env))]
-    [(app proc-expr arg-expr ...) (app? #'app)
+    [(let ([id expr]) body) (identifier? #'id)
+     (peval #'body (bind env #'id (peval #'expr env)))]
+    [(+ expr1 expr2) (+ (peval #'expr1 env) (peval #'expr2 env))]
+    [(proc-expr arg-expr ...)
      (let ([proc (peval #'proc-expr env)]
            [args (map (lambda (stx) (peval stx env)) (syntax->list #'(arg-expr ...)))])
        (partial-apply proc args))]))
@@ -54,5 +42,5 @@
     (for/fold ([env (closure-env proc)])
               ([arg-id (in-list (closure-arg-ids proc))]
                [arg (in-list args)])
-      (bind-id env arg-id arg)))
+      (bind env arg-id arg)))
   (peval (closure-expr proc) eval-env))
